f28ce74ef57c07765596855fda87084c2456d7d7
vineeth <vineeth.kanupuru@servicemaster.com>
2020-10-12 17:10:16 -0500

    dev1, dev3

--- mapping.py
+++ mapping.py
@@ -5,6 +5,7 @@ import os
 supported_environments = ['GT1DEV', 'SW1DEV', 'GT1UAT', 'SW1UAT', 'GT1PERF', 'SW1PERF', 'GT1PROD', 'SW1PROD']
 services_array = ['create-service', 'share-service', 'unshare-service']
 supported_file_types = ['.yml', 'yaml']
+is_dev_uat = False
 
 
 def get_documents(file_path):
@@ -27,8 +28,14 @@ def cf_command(service_name, rlm_id, username, password):
     print(script)
 
 
-def sf_command(service_name, rlm_id, org, space):
-    script = 'cf ' + service_name + ' ' + rlm_id + ' -o ' + org + ' -s ' + space
+def sf_command(service_name, rlm_id, org, space, env_var):
+    script = 'cf ' + service_name + ' ' + rlm_id + ' -o ' + org + ' -s ' + space + '-' + env_var
+    if is_dev_uat:
+        script1 = script + '1' + '\r\n'
+        script3 = script + '3' + '\r\n'
+        for scr in [script1, script3]:
+            txt_file.write(scr)
+        return
     txt_file.write(script + '\r\n')
     print(script)
 
@@ -62,6 +69,11 @@ def check_last_char_if_number_remove(env_variable):
     return env_variable
 
 
+def check_if_dev_uat_environment(env_var):
+    if 'DEV' in env_var or 'UAT' in env_var:
+        return True
+
+
 script_arguments = sys.argv
 if len(script_arguments) <= 1:
     print("Environment and yml files path are required as arguments in that order")
@@ -71,6 +83,8 @@ environment_argument = format_environment_variable(script_arguments[1])
 if environment_argument not in supported_environments:
     print('Given environment: ' + environment_argument + ', is not one of supported environments.')
     sys.exit(0)
+
+is_dev_uat = check_if_dev_uat_environment(environment_argument)
 
 yml_files_path = script_arguments[2]
 if yml_files_path[-1] != '/':
@@ -102,5 +116,5 @@ for yml_file in yml_files_to_parse:
                         if credhub_property == 'share-service' or credhub_property == 'unshare-service':
                             for rml_id, org_space_array in credhub_service.items():
                                 for org_space in org_space_array:
-                                    sf_command(credhub_property, rml_id, org_space['ORG'], org_space['SPACE'])
+                                    sf_command(credhub_property, rml_id, org_space['ORG'], org_space['SPACE'], environment_argument)
 txt_file.close()
